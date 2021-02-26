1d8dd3573fdbc10590050a037d7bac081cf4c68c
Jiachi Liu <inbox@huozhi.im>
2021-02-26 16:18:44 +0800

    stretch layout

--- pages/index.jsx
+++ pages/index.jsx
@@ -7,7 +7,15 @@ const imageStyles = {
 
 export default function App() {
   return (
-    <div style={{margin: 0, background: 'rgb(14, 14, 14)', width: '100vw', height: '100vh'}}>
+    <div 
+      style={{
+        margin: 0, background: 'rgb(14, 14, 14)', width: '100vw', height: '100vh',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'column',
+      }}
+    >
       <div style={{display: 'flex', justifyContent: 'center'}}>
         <img
           style={imageStyles}
