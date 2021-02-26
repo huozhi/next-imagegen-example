9c47b01ce94e87b393d17b35bc16d91ae396f339
Jiachi Liu <inbox@huozhi.im>
2021-02-26 16:16:08 +0800

    add link

--- pages/index.jsx
+++ pages/index.jsx
@@ -7,16 +7,21 @@ const imageStyles = {
 
 export default function App() {
   return (
-    <div style={{margin: 0, background: 'rgb(14, 14, 14)', width: '100vw', height: '100vh', display: 'flex', justifyContent: 'center'}}>
-      <img
-        style={imageStyles}
-        src="/logo/microlink.image"
-      />
-      <div style={{fontSize: '100px', alignSelf: 'center', color: '#fff'}}>&times;</div>
-      <img
-        style={imageStyles}
-        src="/logo/vercel.image"
-      />
+    <div style={{margin: 0, background: 'rgb(14, 14, 14)', width: '100vw', height: '100vh'}}>
+      <div style={{display: 'flex', justifyContent: 'center'}}>
+        <img
+          style={imageStyles}
+          src="/logo/microlink.image"
+        />
+        <div style={{fontSize: '100px', alignSelf: 'center', color: '#fff'}}>&times;</div>
+        <img
+          style={imageStyles}
+          src="/logo/vercel.image"
+        />
+      </div>
+      <a style={{color: '#fff', display: 'block', margin: '40px', textAlign: 'center'}} href="https://github.com/huozhi/imagegen-logo-previews">
+        github.com/huozhi/imagegen-logo-previews
+      </a>
     </div>
   )
 }
