caae21b94368c27f0054ea87d2b89c0f8ab199bd
Jiachi Liu <inbox@huozhi.im>
2021-03-02 23:20:37 +0800

    polish

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,18 @@
+import Link from 'next/link'
+import styles from './index.module.css'
+
 export default function Index() {
   return (
-    <div>
-      {`Hello 👋`}
+    <div className="container">
+      <h1>{`Hello 👋`}</h1>
+      <ul className={styles.ul}>
+        <li>
+          <Link href='/vercel'>
+            <a className={styles.link}>Generate Vercel Logos</a>
+          </Link>
+        </li>
+      </ul>
+        
     </div>
   )
 }
