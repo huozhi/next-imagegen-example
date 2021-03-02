7ee93aac86d9dbfb26f25eba7f1a9629df8fd3e2
Jiachi Liu <inbox@huozhi.im>
2021-03-02 20:43:35 +0800

    gh card

--- pages/api/imagegen/[...imagegen].js
+++ pages/api/imagegen/[...imagegen].js
@@ -3,14 +3,10 @@ import { provider } from 'next-plugin-imagegen-puppeteer'
 
 const options = {
   viewport: {
-    x: 0,
-    y: 0,
-    width: 240,
-    height: 100,
+    width: 1200,
+    height: 630,
   }
 }
-
-console.log(`db:${process.env.NODE_ENV} === 'development'`,)
 
 export default handler(
   process.env.NODE_ENV === 'development' ?
--- pages/github.image.jsx
+++ pages/github.image.jsx
@@ -2,23 +2,40 @@ import styles from './github.image.module.css'
 
 export default function GithubProfile({
   name = 'huozhi', 
-  langs = ['javascript', 'css'],
-  location = 'China',
+  fullname = 'Jiachi Liu',
+  company = 'Home',
+  title = 'Web Developer',
 }) {
   return (
     <div className={styles.root}>
       <div className={styles.card}>
-        
-        <div className={styles.profile}>
-          <img className={styles.avatar} src={`https://github.com/${name}.png`} />
+        <div className={styles.logo}>
+          <svg height={240} viewBox="0 0 148 90" version="1.1" fill="#fff" style={{transform: `translateX(4%)`, shapeRendering: 'auto'}}>
+            <linearGradient id="gradient" x1="5.86667" y1="18.8571" x2="110.978" y2="18.8571" gradientUnits="userSpaceOnUse">
+              <stop stopColor="#EC6192"></stop>
+              <stop offset="0.213542" stopColor="#EC4C34"></stop>
+              <stop offset="0.411458" stopColor="#FFBD2B"></stop>
+              <stop offset="0.630208" stopColor="#EBDE56"></stop>
+              <stop offset="0.833333" stopColor="#57C754"></stop>
+              <stop offset="1" stopColor="#53A1EB"></stop>
+            </linearGradient>
+            <path d="M34.992 23.495h27.855v2.219H37.546v16.699h23.792v2.219H37.546v18.334h25.591v2.219H34.992v-41.69zm30.35 0h2.96l13.115 18.334 13.405-18.334L113.055.207 83.1 43.756l15.436 21.429H95.46L81.417 45.683 67.316 65.185h-3.018L79.85 43.756 65.343 23.495zm34.297 2.219v-2.219h31.742v2.219h-14.623v39.47h-2.554v-39.47H99.64zM.145 23.495h3.192l44.011 66.003L29.16 65.185 2.814 26.648l-.116 38.537H.145v-41.69zm130.98 38.801c-.523 0-.914-.405-.914-.928 0-.524.391-.929.913-.929.528 0 .913.405.913.929 0 .523-.385.928-.913.928zm2.508-2.443H135c.019.742.56 1.24 1.354 1.24.888 0 1.391-.535 1.391-1.539v-6.356h1.391v6.362c0 1.808-1.043 2.849-2.77 2.849-1.62 0-2.732-1.01-2.732-2.556zm7.322-.08h1.379c.118.853.95 1.395 2.149 1.395 1.117 0 1.937-.58 1.937-1.377 0-.685-.521-1.097-1.708-1.377l-1.155-.28c-1.62-.38-2.36-1.166-2.36-2.487 0-1.602 1.304-2.668 3.26-2.668 1.82 0 3.15 1.066 3.23 2.58h-1.354c-.13-.828-.85-1.346-1.894-1.346-1.1 0-1.832.53-1.832 1.34 0 .642.472 1.01 1.64 1.284l.987.243c1.838.43 2.596 1.178 2.596 2.53 0 1.72-1.33 2.799-3.453 2.799-1.987 0-3.323-1.029-3.422-2.637z" fillRule="nonzero"></path>
+          </svg>
+        </div>
+        <div className={styles.col}>
+          <div className={styles.profile}>
+            <img className={styles.avatar} src={`https://github.com/${name}.png`} />
+          </div>
+          <div style={{marginTop: 'auto'}}>
+            <div className={styles.divider} />
+            <div style={{fontSize: 30}}>{`@${name}`}</div>
+          </div>
         </div>
         <div className={styles.info}>
-          <div className={styles.divider} />
-          <div>{`@${name}`}</div>
-          <div>{langs.join(' · ')}</div>
-          <div>
-            <svg viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M11.536 3.464a5 5 0 010 7.072L8 14.07l-3.536-3.535a5 5 0 117.072-7.072v.001zm1.06 8.132a6.5 6.5 0 10-9.192 0l3.535 3.536a1.5 1.5 0 002.122 0l3.535-3.536zM8 9a2 2 0 100-4 2 2 0 000 4z"></path></svg>
-            {location}
+          <div>{fullname}</div>
+          <div style={{fontSize: 24}}>
+            <span style={{color: `#444`}}>{`${title} @ `}</span>
+            <span>{company}</span>
           </div>
         </div>
       </div>
