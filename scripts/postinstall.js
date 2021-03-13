2705c9a6d14751a3144940cc928752e3d2dcf18b
Jiachi Liu <inbox@huozhi.im>
2021-03-13 13:29:39 +0800

    use index.js as main

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -4,9 +4,11 @@ const { resolve } = require('path')
 const cwd = process.cwd()
 const nodeModuleDir = resolve(cwd, 'node_modules')
 const prismPkg = resolve(nodeModuleDir, 'prism-themes', 'package.json')
+const prismMain = resolve(nodeModuleDir, 'prism-themes', 'index.js')
 
-// remove main: README.md from package.json
+// override main: README.md from package.json
 const pkg = require(prismPkg)
-delete pkg.main
+pkg.main = 'index.js'
 
 fs.writeFileSync(prismPkg, JSON.stringify(pkg))
+fs.writeFileSync(prismMain, `throw new Error('no direct import')`)
